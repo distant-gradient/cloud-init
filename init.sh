fcdfaf498b2474c5550386a7e154eb913d8d4d59
distant-gradient <48427774+distant-gradient@users.noreply.github.com>
2019-07-28 19:19:34 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
 sudo apt-get update
-sudo apt-get install -y docker.io virtualbox linux-headers-generic virtualbox-dkms
-sudo docker kill emulator                                                                                                                                                                                            
-sudo docker rm emulator                                                                                                                                                                                              
-sudo docker run --name emulator -it -v /dev/vboxdrv:/dev/vboxdrv vaani/emulator
+sudo apt-get install -y docker.io virtualbox linux-headers-generic virtualbox-dkms                                                                                                                                                                                            
+sudo gcloud docker run --name emulator -it -v /dev/vboxdrv:/dev/vboxdrv vaani/emulator
