c1b0e182ca06195376524422c1934f7fb2ae0da2
distant-gradient <48427774+distant-gradient@users.noreply.github.com>
2019-07-28 20:28:06 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get update
 sudo apt-get install -y docker.io virtualbox linux-headers-generic virtualbox-dkms                                                                                                                                                                                            
-sudo gcloud docker run --name emulator -it -v /dev/vboxdrv:/dev/vboxdrv vaani/emulator
+sudo gcloud docker run --name emulator -it -v /dev/vboxdrv:/dev/vboxdrv us.gcr.io/stellar-works-234006/wa-em
