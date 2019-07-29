da9fb1b41ab3551da13ec9bdd1f038be7b813fc0
distant-gradient <48427774+distant-gradient@users.noreply.github.com>
2019-07-29 15:06:20 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 sudo apt-get update
-sudo apt-get install -y docker.io virtualbox linux-headers-generic virtualbox-dkms                                                                                                                                                                                            
+sudo apt-get install -y docker.io virtualbox linux-headers-generic virtualbox-dkms   
+sudo modprobe vboxdrv
 sudo gcloud docker -- run --name emulator -it -p 6901:6901 -v /dev/vboxdrv:/dev/vboxdrv us.gcr.io/stellar-works-234006/wa-em &
